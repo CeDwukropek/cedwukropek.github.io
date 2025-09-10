f33cf73e38efbe2cfd9c9ce33d9eb014f46d3934
CeDwukropek <emilkopytek@gmail.com>
2025-09-10 18:43:16 +0200

    fix routing

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Header darkMode={darkMode} setDarkMode={setDarkMode} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/filament/:id" element={<FilamentDetail />} />
+        <Route path="/filaments/:id" element={<FilamentDetail />} />
       </Routes>
     </div>
   );
